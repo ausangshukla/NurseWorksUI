8d1c04539330444961c3955d6ed3dbf44bb9c23c
Mohith Thimmaiah <thimmaiah@gmail.com>
2020-06-18 11:44:01 +0530

    Better nurse search

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { UserApi } from '../../providers/user-api';
 import { ResponseUtility } from '../../providers/response-utility';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { filterQueryId } from '@angular/core/src/view/util';
 
 /**
  * Generated class for the Users page.
@@ -17,23 +19,51 @@ import { ResponseUtility } from '../../providers/response-utility';
 export class Users {
 
   users: any;
-  user: any;
+  user: {};
+  filter: {} 
+  slideOneForm: FormGroup;
+  showFilter;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public loadingController: LoadingController,
+    public loadingController: LoadingController, public formBuilder: FormBuilder,
     public userApi: UserApi, public respUtility: ResponseUtility) {
+
+      this.slideOneForm = formBuilder.group({
+        key_qualifications: [''],
+        specializations: [''],
+        experience: [''],
+        availability: [''],
+      });
+
+      this.filter = {"specializations": "", "key_qualifications": "", "experience": "", "availability": ""};
+      this.showFilter = false;
+  }
+
+  openFilter() {
+    this.showFilter = !(this.showFilter);
+    this.filter = {};
+    console.log("openFilter = " + this.showFilter);
   }
 
   ionViewWillEnter() {
     console.log('ionViewWillEnter Users');
     this.respUtility.trackView("Users");
+    this.search();
+  }
+
+  search() {
     let loader = this.loadingController.create({
       content: 'Loading Users..'
     });
 
     loader.present();
+    for (var key in this.filter) {
+      if (this.filter[key] == undefined) {
+        this.filter[key] = "";
+      }
+    }
 
-    this.userApi.getUsers({}).subscribe(
+    this.userApi.getUsers(this.filter).subscribe(
       users => {
         this.users = users;
         console.log("Loaded users");
@@ -44,7 +74,6 @@ export class Users {
     );
 
   }
-
   getUserDetails(user) {
     this.respUtility.trackEvent("User", "Details", "click");
     this.navCtrl.push('UserDetails', user);
